8ea22fef31f6db33aa37c14e80fc6d84b1d836c0
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-12-30 21:51:33 -0500

    Add themeAtom and rowAtoms to state.ts

--- src/state.ts
+++ src/state.ts
@@ -1,3 +1,11 @@
 import { atom } from 'jotai'
 
 export const themeAtom = atom('dark');
+
+export const activeRowIndexAtom = atom(0);
+export const row1Atom = atom<string[]>([])
+export const row2Atom = atom<string[]>([])
+export const row3Atom = atom<string[]>([])
+export const row4Atom = atom<string[]>([])
+export const row5Atom = atom<string[]>([])
+
