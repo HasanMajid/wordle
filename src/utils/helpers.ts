0cb274755b521402ada015e3bbe8e6fd60db4037
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-12-31 15:35:09 -0500

    Make checkWord function use GET API endpoint and pass query parameter

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,13 +1,12 @@
 export const checkWord = async (word: string) => {
-    fetch("/api/check", {
-        body: JSON.stringify({word: word})
-    }).then(response => {
-        response.json().then(message => {
-            console.log(message);
+    fetch("/api/check/" + word)
+        .then(response => {
+            response.json().then(message => {
+                console.log(message);
+            }).catch(err => {
+                console.log('error fetching');
+            })
         }).catch(err => {
-            console.log('error fetching');
+            console.log('error');
         })
-    }).catch(err => {
-        console.log('error');
-    })
 }
