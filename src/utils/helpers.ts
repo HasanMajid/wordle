c24a4792b61646649e3af58ef39ff51318de06c6
Hasan Majid <hasanmajid3.14@hotmail.com>
2024-01-01 08:26:39 -0500

    Refactor checkWord function to use async/await syntax

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,12 +1,11 @@
-export const checkWord = async (word: string) => {
-    fetch("/api/check/" + word)
-        .then(response => {
-            response.json().then(message => {
-                console.log(message);
-            }).catch(err => {
-                console.log('error fetching');
-            })
-        }).catch(err => {
-            console.log('error');
-        })
+export async function checkWord(word: string): Promise<null | string[]> {
+    try {
+        const response = await fetch("/api/check/" + word)
+        const data = await response.json();
+        return data.correctness;
+    } catch (err) {
+        console.log("error checking word", err);
+        return null;
+    }
+
 }
