392766590b8a23d6cdb0f09bbf72b398bf126533
Hasan Majid <hasanmajid3.14@hotmail.com>
2024-01-01 08:35:14 -0500

    Remove unused import in state.ts

--- src/state.ts
+++ src/state.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { atom, useAtom } from "jotai";
 import { Atom, PrimitiveAtom } from "jotai/vanilla";
 import { checkWord } from "./utils/helpers";
