cb11ae812cfde4c8c6a05d32e0e0c26de5eeb791
Hasan Majid <hasanmajid3.14@hotmail.com>
2024-01-02 17:22:34 -0500

    Add win state to Grid component

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -1,14 +1,18 @@
 "use client"
+import { useAtom } from 'jotai';
 import Row from './row/Row'
-import { rows, rowColoursAtoms, useGridAtom } from '@/state'
+import { rows, rowColoursAtoms, useGridAtom, winStateAtom } from '@/state'
 
 function Grid() {
-
+    const [winState] = useAtom(winStateAtom);
     const { isChecking } = useGridAtom();
 
 
     return (
         <>
+            <div>
+                {winState ? <h2 className='heading' style={{ width: "fit-content", height: "fit-content", margin: "auto" }}>You Win</h2> : null}
+            </div>
             {rows.map((rowAtom, index) => {
                 return <Row rowAtom={rowAtom} rowColoursAtom={rowColoursAtoms[index]} rowIndex={index} key={index} />
             })}
--- src/state.ts
+++ src/state.ts
@@ -4,7 +4,7 @@ import { Atom, PrimitiveAtom } from "jotai/vanilla";
 import { checkWord } from "./utils/helpers";
 
 export const themeAtom = atom<"dark" | "light">("dark");
-
+export const winStateAtom = atom<boolean>(false);
 export const activeRowIndexAtom = atom(0);
 export const maxRowLength = 5;
 
@@ -37,6 +37,7 @@ export const useRowAtom = (rowAtom: PrimitiveAtom<string[]>, rowIndex: number) =
     const [isChecking, setIsChecking] = useAtom(isCheckingAtom);
     const [activeRowIndex, setActiveRowIndex] = useAtom(activeRowIndexAtom)
     const [row, setRow] = useAtom(rowAtom);
+    const [winState, setWinState] = useAtom(winStateAtom);
 
     useEffect(() => {
         const handleKeyDown = (e: KeyboardEvent) => {
@@ -59,13 +60,13 @@ export const useRowAtom = (rowAtom: PrimitiveAtom<string[]>, rowIndex: number) =
             }
         }
 
-        if (activeRowIndex === rowIndex && isChecking === false) {
+        if (activeRowIndex === rowIndex && isChecking === false && winState === false) {
             document.addEventListener("keydown", handleKeyDown);
         }
         return () => {
             document.removeEventListener("keydown", handleKeyDown);
         }
-    }, [activeRowIndex, isChecking, row.length, rowIndex, setRow])
+    }, [activeRowIndex, isChecking, row.length, rowIndex, setRow, winState])
 }
 
 export const useGridAtom = () => {
@@ -75,7 +76,8 @@ export const useGridAtom = () => {
     const [row, setRow] = useAtom(activeRowAtom);
     const rowColoursAtom = rowColoursAtoms[activeRowIndex];
     const [rowColours, setRowColours] = useAtom(rowColoursAtom);
-
+    const [winState, setWinState] = useAtom(winStateAtom)
+    
     useEffect(() => {
         const handleEnterDown = async (e: KeyboardEvent) => {
             let word = ""
@@ -88,6 +90,10 @@ export const useGridAtom = () => {
                 const correctness = await checkWord(word);
                 if (correctness !== null) {
                     setRowColours(correctness);
+                    if (correctness.every((value) => value === "green")) {
+                        console.log('setting win state');
+                        setWinState(true);
+                    }
                     if (activeRowIndex !== 5 && row.length === maxRowLength) {
                         setActiveRowIndex(prev => prev + 1)
                     }
@@ -97,15 +103,14 @@ export const useGridAtom = () => {
             }
         }
 
-        if (row.length === maxRowLength && isChecking === false) {
-            // TODO: check if word is right and display win state
+        if (row.length === maxRowLength && isChecking === false && winState === false) {
             document.addEventListener("keydown", handleEnterDown);
         }
 
         return () => {
             document.removeEventListener("keydown", handleEnterDown);
         }
-    }, [activeRowIndex, isChecking, row, setActiveRowIndex, setIsChecking, setRowColours])
+    }, [activeRowIndex, isChecking, row, winState, setActiveRowIndex, setIsChecking, setRowColours, setWinState])
 
     return { isChecking }
 }
