ec3efec01c8864bfd580e854d2960e63419181dc
Hasan Majid <hasanmajid3.14@hotmail.com>
2024-01-02 20:13:47 -0500

    Add time display in Grid component using useTime hook

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -1,17 +1,19 @@
 "use client"
 import { useAtom } from 'jotai';
 import Row from './row/Row'
-import { rows, rowColoursAtoms, useGridAtom, winStateAtom } from '@/state'
+import { rows, rowColoursAtoms, useGridAtom, winStateAtom, useTime } from '@/state'
 
 function Grid() {
     const [winState] = useAtom(winStateAtom);
     const { isChecking } = useGridAtom();
 
+    const { hours, minutes, loadingTime } = useTime()
 
     return (
         <>
-            <div>
-                {winState ? <h2 className='heading' style={{ width: "fit-content", height: "fit-content", margin: "auto" }}>You Win</h2> : null}
+            <div style={{ display: "flex", flexDirection: "column", gap: "1rem", marginBottom: "1rem" }}>
+                {!loadingTime ? <h4 className='heading' style={{ width: "fit-content", height: "fit-content", margin: "auto" }}>{hours} hours and {minutes} until next word</h4> : null}
+                {winState ? <h2 className='heading' style={{ width: "fit-content", height: "fit-content", margin: "auto" }}>You Win</h2> : <h2></h2>}
             </div>
             {rows.map((rowAtom, index) => {
                 return <Row rowAtom={rowAtom} rowColoursAtom={rowColoursAtoms[index]} rowIndex={index} key={index} />
